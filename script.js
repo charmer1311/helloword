4057f7b0df97378a800bf5217f1463317cedafd8
Viktoria <visharipova@yandex.ru>
2025-07-09 22:18:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const formFilter = document.querySelector('.filtering-form');
+const btnFilter = document.querySelector('.select-filter1');
+
+
+
+btnFilter.addEventListener("click", () => {
+    formFilter.classList.toggle('show');
+});
